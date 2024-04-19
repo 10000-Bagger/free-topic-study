711c53619b38187998a29cec3524df87c08a7bd8
binary_ho <dfghcvb11@naver.com>
2024-04-20 01:15:20 +0900

    Update count_files.py

--- utils/count_files.py
+++ utils/count_files.py
@@ -51,20 +51,28 @@ def count_files():
   
   
 def make_info(files_info, total_file_count):
-    info = f"## Files Count In Folders\nTotal File Count: {total_file_count}\n"
-    for directory_files_info in files_info:
-        temp = f"- {directory_files_info[0]}: {directory_files_info[1]}\n"
-        info += temp
+    info = f"### 전체 아티클 갯수: {total_file_count}개"
+    # for directory_files_info in files_info:
+    #     temp = f"- {directory_files_info[0]}: {directory_files_info[1]}\n"
+    #     info += temp
     return info
     
-
-
 def make_read_me(info):
-    return f"""# Self-Updating-Readme
-Push할 때마다 폴더 별 파일 수를 리드미에 자동으로 업데이트<br>
-Automatically update the number of files per folder to Readme whenever you push.<br><br>
+    return f"""# 자유 주제 스터디
 {info}
+### 진행 방식
+- 각자 이름의 branch에서 각자 이름의 폴더에 공부 내용을 정리하여 한다.
+- 일주일에 5번 main branch를 향하는 PR을 올린다.
+  - 새벽 6시 전까지 올린 PR은 전날 올린 건으로 간주된다. (출근길 글 읽기를 위해)
+- 매일 낮 12시 이전까지 스터디원이 올린 PR에 승인, 질문 등의 피드백을 남긴다.
+- PR에 꼭 완성된 글을 올리지 않아도 된다.
 """
+
+#     return f"""# Self-Updating-Readme
+# Push할 때마다 폴더 별 파일 수를 리드미에 자동으로 업데이트<br>
+# Automatically update the number of files per folder to Readme whenever you push.<br><br>
+# {info}
+# """
 
 
 def update_readme():
